1066b1ce7c4bd77b2b166689599b6d3dddf04943
sergioluna <sluna@sergioluna.dev>
2021-12-21 16:36:38 -0800

    Update App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome!/i);
   expect(linkElement).toBeInTheDocument();
