b0fbc1af152f88898e8e0589cfe03fef58347255
sergioluna <sluna@sergioluna.dev>
2021-12-31 17:03:38 -0800

    Add collapsable links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,34 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (        
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <div className="navbar-brand">Palms Park Bridge Club</div>
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNavDropdown" 
+                    aria-controls="navbarNavDropdown" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/registration">Registration</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/score-calculator">Score Calculator</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
