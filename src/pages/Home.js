62ecef6f2813f3d538e2ccd0708a2622094515d4
sergioluna <sluna@sergioluna.dev>
2022-01-03 13:46:31 -0800

    Update text on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="container p-5">
-            <p className="fs-3">Welcome to Palms Park Bridge</p>
+            <p className="fs-2">Welcome to Palms Park Bridge</p>
             <p>We are happy that you stopped by today!</p>
-            <p>Did you know that according to experts, Bridge is a Mental "Sport?"</p>
+            <p>Did you know that according to experts, Bridge is a "Mental Sport?"</p>
             <p>With that said...</p>
             <p>All Mental Atheletes are welcome here.</p>
             <p>Enter for <Link to="registration">registration</Link> to our next event and access to our <Link to="score-calculator">scoring calculator</Link></p>
