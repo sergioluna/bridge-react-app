ca9fd5ddd8bd8d62e221c08507c857d65e0bee02
sergioluna <sluna@sergioluna.dev>
2022-01-03 13:46:47 -0800

    Add text to Registration page

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,7 +1,14 @@
 function Registration() {
     return (
         <div className="container p-5">
-            TODO
+            <p className="fs-2">Registration Page</p>
+            <p className="fs-4">Want to join us?</p>
+            <p>We play duplicate bridge twice a week:</p>
+            <p> <strong>Monday & Thursday from 9am till about 12:30, live at <i>12305 207th Street Lakewood</i></strong></p>
+            <p>
+                You need to register at least 24 hours in advance for the following week.
+                Please complete the reservation form below or call us at <a href="tel:+15622106265">(562) 210-6265</a>
+            </p>
         </div>
     );
 }
