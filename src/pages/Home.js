51427e0cf07fef5c8029aba4b15e4a3c8a229133
sergioluna <sluna@sergioluna.dev>
2022-01-03 15:07:01 -0800

    Add registration form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="container p-5">
-            <p className="fs-2">Welcome to Palms Park Bridge</p>
-            <p>We are happy that you stopped by today!</p>
+            <p className="fs-2">Welcome to Palms Park Bridge!</p>
+            <p>We are happy that you stopped by today.</p>
             <p>Did you know that according to experts, Bridge is a "Mental Sport?"</p>
             <p>With that said...</p>
             <p>All Mental Atheletes are welcome here.</p>
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,14 +1,34 @@
+import '../components/RegistrationForm';
+import RegistrationForm from '../components/RegistrationForm';
+
 function Registration() {
     return (
         <div className="container p-5">
-            <p className="fs-2">Registration Page</p>
-            <p className="fs-4">Want to join us?</p>
-            <p>We play duplicate bridge twice a week:</p>
-            <p> <strong>Monday & Thursday from 9am till about 12:30, live at <i>12305 207th Street Lakewood</i></strong></p>
+            <p class="fs-2">Registration</p>
             <p>
-                You need to register at least 24 hours in advance for the following week.
-                Please complete the reservation form below or call us at <a href="tel:+15622106265">(562) 210-6265</a>
+                Are you ready to play? We play duplicate bridge twice a week
+                (Monday & Thursday from 9am till about 12:30), live at:
             </p>
+                
+            <p>
+                <i>12305 207th Street Lakewood</i>
+            </p>
+
+            <p>
+                Want to join us? You need to register at least 24 hours in
+                advance for the following week.
+            </p>
+
+            <p>
+                Please complete the reservation form below or call us at: 
+                <a href="tel:+15622106265">+1 (562) 210-6265</a>
+            </p>
+
+            <p>
+                We will confirm your seat availability within a few hours.
+            </p>
+
+            <RegistrationForm />
         </div>
     );
 }
